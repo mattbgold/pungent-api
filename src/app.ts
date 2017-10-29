7ed3edc8c4f8a603cba368a62900a737a4c9e98c
Matt Gold <mgold@kabbage.com>
2017-10-29 19:56:25 -0400

    update pungent ver. split response by line. pass pungent config in post body

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,35 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 import { PunGenerator } from 'pungent';
+
+import * as byline from 'byline';
 
 export function bootstrapApi(): any {
     const app = express();
+    app.use(bodyParser.json({limit: '100mb'}));
+
     const router = express.Router();
     
-    router.get('/pun', (req, res) => {
-        const punWord = req.query.punWord;
-        const phrase = req.query.phrase;
+    router.post('/pun', (req, res) => {
+        const punWord: string = req.body.punWord;
+        const phrase: string = req.body.phrase;
 
-        const generator = new PunGenerator();
-        
-        var puns = generator.generatePuns(phrase, punWord);
+        req.body.options = req.body.options || {};
 
-        res.json({
-            result: puns.map(p => p.syllables.map(s => s.text).join('')).join(' ')
+        const generator = new PunGenerator({
+            punFrequency: req.body.options.punFrequency || 1,
+            punScoreTolerance: req.body.options.punScoreTolerance || .5,
+            replacementTolerance: req.body.options.replacementTolerance || .5
         });
+
+        let resultLines: string[] = 
+            phrase.split('\n')
+                .map(line => generator.generatePuns(line, punWord)
+                    .map(p => p.word)
+                    .join(' ')
+                );
+
+        res.json({ resultLines });
     });
 
     app.use('/api', router);
